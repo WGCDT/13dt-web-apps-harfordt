52a4122b7692b1a745a11218931de77a688fa94c
harfordt <tim.harford@wgc.school.nz>
2019-03-26 22:56:00 +1300

    Created templates and routes for main pages

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home_page():
-    return "<h1>Hello world!!!</h1>"
+    return render_template("home.html")
 
+@app.route('/products')
+def products_page():
+    return render_template("products.html")
+
+@app.route('/contact')
+def contact_page():
+    return render_template("contact.html")
+
+@app.route('/register')
+def register_page():
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
