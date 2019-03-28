5dbe20a62fb4282dffa28ba028cf95cf897490c5
harfordt <tim.harford@wgc.school.nz>
2019-03-28 14:48:41 +1300

    Added trello routes, WGC compatibility

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append("N:\python-modules")
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,17 +10,26 @@ app = Flask(__name__)
 def home_page():
     return render_template("home.html")
 
+
 @app.route('/products')
 def products_page():
     return render_template("products.html")
+
 
 @app.route('/contact')
 def contact_page():
     return render_template("contact.html")
 
+
 @app.route('/register')
 def register_page():
     return render_template("register.html")
+
+
+@app.route('/trellos')
+def trellos():
+    return render_template("trellos.html")
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
